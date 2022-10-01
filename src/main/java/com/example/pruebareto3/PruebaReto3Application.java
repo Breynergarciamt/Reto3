fef5b17061c17f687a1e43b57636e30e52e8e81c
Breynergarciamt <breyner.garcia.mt@usa.edu.com$ git config --global user.email breyner.garcia.mt@usa.edu.com>
2022-10-01 17:29:41 -0500

    first commit

---
+++ src/main/java/com/example/pruebareto3/PruebaReto3Application.java
@@ -0,0 +1,13 @@
+package com.example.pruebareto3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaReto3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaReto3Application.class, args);
+    }
+
+}
